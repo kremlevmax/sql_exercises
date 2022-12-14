867d5c38b94672856516bb36f1480fd31627b81d
kremlevmax <kremlevmax.webdev@icloud.com>
2022-12-14 10:59:12 -0800

    Code-Along: Build the Vinyl Record Shop Database DONE

--- VinylRecordShop.sql
+++ VinylRecordShop.sql
@@ -2,6 +2,7 @@ DROP DATABASE IF EXISTS vinylrecordshop;
 CREATE DATABASE vinylrecordshop;
 USE vinylrecordshop;
 
+DROP TABLE IF EXISTS album;
 CREATE TABLE album (
     albumId INT AUTO_INCREMENT,
     albumTitle VARCHAR(100) NOT NULL,
@@ -13,23 +14,66 @@ CREATE TABLE album (
 );
 DESCRIBE album;
 
+DROP TABLE IF EXISTS artist;
 CREATE TABLE artist (
-artistId INT AUTO_INCREMENT,
-artistFirstName VARCHAR(25),
-artistLastName VARCHAR(50),
-CONSTRAINT pk_artist 
+	artistId INT AUTO_INCREMENT,
+	artistFirstName VARCHAR(25),
+	artistLastName VARCHAR(50),
+	CONSTRAINT pk_artist 
         PRIMARY KEY (artistId)
 );
 DESCRIBE artist;
 
+DROP TABLE IF EXISTS band;
 CREATE TABLE band (
-bandId INT AUTO_INCREMENT,
-bandName VARCHAR(50),
-CONSTRAINT pk_band 
+	bandId INT AUTO_INCREMENT,
+	bandName VARCHAR(50),
+	CONSTRAINT pk_band 
         PRIMARY KEY (bandId)
 );
 DESCRIBE band;
 
+DROP TABLE IF EXISTS song;
 CREATE TABLE song (
-)
+	songId INT NOT NULL AUTO_INCREMENT,
+	songTitle VARCHAR(100) NOT NULL,
+	videoUrl VARCHAR(100),
+	bandId INT NOT NULL,
+	CONSTRAINT pk_song 
+		PRIMARY KEY (songId),
+	CONSTRAINT fk_song_band
+		FOREIGN KEY (bandID)
+        REFERENCES band(bandId)
+);
+DESCRIBE song;
 
+DROP TABLE IF EXISTS songAlbum;
+CREATE TABLE songAlbum (
+	songId INT,
+    albumId INT,
+    CONSTRAINT pk_songAlbum
+		PRIMARY KEY (songId, albumId),
+	CONSTRAINT fk_songAlbum_song
+		FOREIGN KEY (songId)
+        REFERENCES song(songId),
+	CONSTRAINT fk_songAlbum_album
+		FOREIGN KEY (albumId)
+        REFERENCES album(albumId)
+);
+DESCRIBE songAlbum;
+
+
+DROP TABLE IF EXISTS bandArtist;
+CREATE TABLE bandArtist (
+	bandId INT,
+    artistId INT,
+    CONSTRAINT pk_bandArtist
+		PRIMARY KEY (bandId, artistId),
+	CONSTRAINT fk_bandArtist_band
+		FOREIGN KEY (bandId)
+        REFERENCES band(bandId),
+	CONSTRAINT fk_bandArtist_artistId
+		FOREIGN KEY (artistId)
+        REFERENCES artist(artistId)
+);
+DESCRIBE bandArtist;
