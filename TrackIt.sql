1e011f7b1869ae60c050596e70e50fbeb4da8ebe
kremlevmax <kremlevmax.webdev@icloud.com>
2022-12-15 03:13:12 -0800

    TrackIt Database Data Management Exercise DONE

--- TrackIt.sql
+++ TrackIt.sql
@@ -23,6 +23,7 @@ CREATE TABLE ProjectWorker (
     ProjectId CHAR(50) NOT NULL,
     WorkerId INT NOT NULL,
     PRIMARY KEY pk_ProjectWorker (ProjectId, WorkerId),
+    
     FOREIGN KEY fk_ProjectWorker_Project (ProjectId)
         REFERENCES Project(ProjectId),
     FOREIGN KEY fk_ProjectWorker_Worker (WorkerId)
@@ -40,3 +41,91 @@ CREATE TABLE Task (
     FOREIGN KEY fk_Task_ProjectWorker (ProjectId, WorkerId)
         REFERENCES ProjectWorker(ProjectId, WorkerId)
 );
+
+INSERT INTO Worker (WorkerId, FirstName, LastName)
+    VALUES (1, 'Rosemonde', 'Featherbie');
+    
+INSERT INTO Worker (FirstName, LastName)
+    VALUES ('Kingsly', 'Besantie');
+    
+INSERT INTO Worker (FirstName, LastName) VALUES
+    ('Goldi','Pilipets'),
+    ('Dorey','Rulf'),
+    ('Panchito','Ashtonhurst');
+    
+INSERT INTO Worker (WorkerId, FirstName, LastName)
+    VALUES (50, 'Valentino', 'Newvill');
+    
+INSERT INTO Worker (FirstName, LastName)
+    VALUES ('Violet', 'Mercado');
+
+INSERT INTO Project (ProjectId, `Name`, DueDate)
+    VALUES ('db-milestone', 'Database Material', '2018-12-31'); 
+    
+INSERT INTO ProjectWorker (ProjectId, WorkerId)
+    VALUES ('db-milestone', 2);
+    
+INSERT INTO Project (ProjectId, `Name`, DueDate)
+	VALUES ('kitchen', 'Kitchen Remodel', '2025-07-15'); 
+    
+INSERT INTO ProjectWorker (ProjectId, WorkerId) VALUES 
+    ('db-milestone', 1), -- Rosemonde, Database
+    ('kitchen', 2),      -- Kingsly, Kitchen
+    ('db-milestone', 3), -- Goldi, Database
+    ('db-milestone', 4); -- Dorey, Database
+    
+-- Provide a Project Summary and change the DueDate.
+UPDATE Project SET
+    Summary = 'All lessons and exercises for the relational database milestone.',
+    DueDate = '2018-10-15'
+WHERE ProjectId = 'db-milestone';
+
+-- Change Kingsly's LastName to 'Oaks'.
+UPDATE Worker SET
+    LastName = 'Oaks'
+WHERE WorkerId = 2;
+
+UPDATE ProjectWorker SET
+    WorkerID = '5'
+WHERE WorkerId = 2;
+
+-- Disable safe updates.
+SET SQL_SAFE_UPDATES = 0;
+
+-- Deactivate active Projects from 2017.
+UPDATE Project SET
+    IsActive = 0
+WHERE DueDate BETWEEN '2017-01-01' AND '2017-12-31'
+AND IsActive = 1;
+
+-- Enable safe updates.
+SET SQL_SAFE_UPDATES = 1;
+    
+SELECT * FROM Worker;
+SELECT * FROM Project;
+SELECT * FROM ProjectWorker;
+
+DELETE FROM Worker
+WHERE WorkerId = 50;
+
+SELECT * FROM Worker;
+
+SET SQL_SAFE_UPDATES = 0;
+
+-- Delete Tasks first because Task references ProjectWorker.
+DELETE FROM Task
+WHERE WorkerId = 5;
+
+-- Delete ProjectWorker next. 
+-- That removes Kingsly from all Projects.
+DELETE FROM ProjectWorker
+WHERE WorkerId = 5;
+
+-- Finally, remove Panchito.
+DELETE FROM Worker
+WHERE WorkerId = 5;
+
+SET SQL_SAFE_UPDATES = 1;
+
+SELECT * FROM Worker;
+
